653eaeb311c49f96f52b48d47f38bd800eefba3a
winirrr <stephenwinzy@gmail.com>
2022-11-01 07:17:48 +0700

    hello

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module'
+import { UserModule } from './user/user.module';
+import config from './config/keys';
+
+
+
+
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/todo'), TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodoModule, AuthModule,UserModule,MongooseModule.forRoot(config.mongoURI)],
+  controllers: [AppController,],
+  providers: [AppService,],
 })
 export class AppModule {}
---
+++ src/config/keys.ts
@@ -0,0 +1,3 @@
+export default{
+    mongoURI: "mongodb+srv://test:masrid3r@test.v5fhwhj.mongodb.net/?retryWrites=true&w=majority"
+}
