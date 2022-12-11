8b55f5ef9089e70680bef7082f8b629b4e5a8a3a
J.Phaiboon <phaiboon760@gmail.com>
2022-12-12 06:24:50 +0700

    disable JWTGuard

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,19 +6,19 @@ import {
   Patch,
   Param,
   Delete,
-  Req,
-  UseGuards,
+  // Req,
+  // UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import RequestWithUser from 'src/auth/requestWithUser.interface';
-import { JwtGuard } from 'src/auth/guards/jwt.guard';
+// import RequestWithUser from 'src/auth/requestWithUser.interface';
+// import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
-  @UseGuards(JwtGuard)
+  // @UseGuards(JwtGuard)
   @Post()
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(createTaskDto);
@@ -28,17 +28,17 @@ export class TasksController {
   findAll() {
     return this.tasksService.findAll();
   }
-  @UseGuards(JwtGuard)
+  // @UseGuards(JwtGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tasksService.findOne(id);
   }
-  @UseGuards(JwtGuard)
+  // @UseGuards(JwtGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(id, updateTaskDto);
   }
-  @UseGuards(JwtGuard)
+  // @UseGuards(JwtGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.tasksService.remove(id);
