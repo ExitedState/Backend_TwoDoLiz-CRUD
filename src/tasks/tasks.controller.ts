224eba980ce658d029d2b65b6a0d00b7dc4148ec
J.Phaiboon <phaiboon760@gmail.com>
2022-12-12 07:24:04 +0700

    remove unused import

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,19 +6,15 @@ import {
   Patch,
   Param,
   Delete,
-  // Req,
-  // UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-// import RequestWithUser from 'src/auth/requestWithUser.interface';
-// import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
-  // @UseGuards(JwtGuard)
+
   @Post()
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(createTaskDto);
@@ -28,17 +24,17 @@ export class TasksController {
   findAll() {
     return this.tasksService.findAll();
   }
-  // @UseGuards(JwtGuard)
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tasksService.findOne(id);
   }
-  // @UseGuards(JwtGuard)
+
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(id, updateTaskDto);
   }
-  // @UseGuards(JwtGuard)
+
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.tasksService.remove(id);
